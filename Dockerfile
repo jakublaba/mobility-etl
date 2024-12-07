6e1388ce4c2c919f770f168dec2b466a63408762
jakublaba <jakub.maciej.laba@gmail.com>
2024-12-07 13:28:37 +0100

    Fix spark task

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM quay.io/astronomer/astro-runtime:12.5.0
 USER root
 
 RUN apt-get update && \
-apt-get install -y ant openjdk-17-jdk && \
+apt-get install -y openjdk-17-jdk && \
 apt-get clean
 
 ENV JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/
--- dags/warsaw-gtfs.py
+++ dags/warsaw-gtfs.py
@@ -11,7 +11,9 @@ from pyspark.sql import SparkSession
 
 dotenv.load_dotenv()
 GTFS_FEED_URL = os.getenv("GTFS_FEED_URL")
-ABS_CONN_STR = os.getenv("ABS_CONNECTION_STRING")
+AZURE_STORAGE_ACCOUNT_NAME = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
+AZURE_STORAGE_ACCESS_KEY = os.getenv("AZURE_STORAGE_ACCESS_KEY")
+AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 GTFS_BUCKET = "gtfs"
 
 
@@ -23,6 +25,8 @@ GTFS_BUCKET = "gtfs"
     catchup=False
 )
 def warsaw_gtfs():
+    gtfs_dir = "/tmp/gtfs"
+    zip_path = f"{gtfs_dir}/gtfs.zip"
     gtfs_files = [
         "agency",
         "calendar_dates",
@@ -35,41 +39,39 @@ def warsaw_gtfs():
     ]
 
     @task
-    def fetch_gtfs_feed() -> str:
+    def fetch_gtfs_feed():
         res = requests.get(GTFS_FEED_URL)
         res.raise_for_status()
-        zip_path = "/tmp/gtfs/warsaw.zip"
         os.makedirs(os.path.dirname(zip_path), exist_ok=True)
         with open(zip_path, "wb") as f:
             f.write(res.content)
-        return zip_path
 
     @task
-    def unzip_gtfs(zip_path: str) -> str:
-        gtfs_dir = os.path.dirname(zip_path)
+    def unzip_gtfs():
         with ZipFile(zip_path, "r") as z:
             z.extractall(gtfs_dir)
-        return gtfs_dir
 
-    @task.pyspark(conn_id="spark-conn")
+    @task.pyspark(conn_id="spark")
     def combine_with_existing(
+            file_name: str,
             spark: SparkSession,
-            _sc: SparkContext,
-            gtfs_dir: str,
-            file_name: str
+            sc: SparkContext,  # noqa: F841
     ):
-        blob_url = (BlobServiceClient
-                    .from_connection_string(ABS_CONN_STR)
-                    .get_blob_client(GTFS_BUCKET, file_name)
-                    .url)
-        existing = spark.read.parquet(blob_url)
+        spark.conf.set(
+            f"fs.azure.account.key.{AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net",
+            AZURE_STORAGE_ACCESS_KEY
+        )
+        blob_exists = (BlobServiceClient
+                       .from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
+                       .get_blob_client(GTFS_BUCKET, file_name)
+                       .exists())
+        path = f"wasbs://gtfs@{AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net/{file_name}"
+        existing = spark.read.parquet(path) if blob_exists else spark.createDataFrame([])
         new = spark.read.csv(os.path.join(gtfs_dir, f"{file_name}.txt"))
         combined = existing.union(new).dropDuplicates()
-        combined.write.mode("overwrite").parquet(blob_url)
+        combined.write.mode("overwrite").parquet(path)
 
-    zip_path_ = fetch_gtfs_feed()
-    gtfs_dir_ = unzip_gtfs(zip_path_)
-    combine_with_existing.partial(gtfs_dir=gtfs_dir_).expand(file_name=gtfs_files)
+    fetch_gtfs_feed() >> unzip_gtfs() >> combine_with_existing.expand(file_name=gtfs_files)
 
 
 warsaw_gtfs()
